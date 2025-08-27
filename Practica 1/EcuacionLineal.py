ffaf15bf13ef600348a2799488e2993bfbb60d68
Alison Diaz Huayhua <adiaz4@umsa.bo>
2025-08-26 19:23:13 -1200

    Update EcuacionLineal.py

--- Practica 1/EcuacionLineal.py
+++ Practica 1/EcuacionLineal.py
@@ -1,20 +1,20 @@
 class EcuacionLineal:
     def __init__(self, a, b, c, d, e, f):
-        self._a = a
-        self._b = b
-        self._c = c
-        self._d = d
-        self._e = e
-        self._f = f
+        self.__a = a
+        self.__b = b
+        self.__c = c
+        self.__d = d
+        self.__e = e
+        self.__f = f
 
     def tieneSolucion(self):
-        return (self._a * self._d - self._b * self._c) != 0
+        return (self.__a * self.__d - self.__b * self.__c) != 0
 
     def getX(self):
-        return (self._e * self._d - self._b * self._f) / (self._a * self._d - self._b * self._c)
+        return (self.__e * self.__d - self.__b * self.__f) / (self.__a * self.__d - self.__b * self.__c)
 
     def getY(self):
-        return (self._a * self._f - self._e * self._c) / (self._a * self._d - self._b * self._c)
+        return (self.__a * self.__f - self.__e * self.__c) / (self.__a * self.__d - self.__b * self.__c)
 
 print("Ingrese a, b, c, d, e, f:")
 valores = list(map(float, input().split()))
